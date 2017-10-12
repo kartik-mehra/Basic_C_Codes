3f877c257457a8bf9f26b3f860d7684496ea081b
Kartik mehra <kartikmhr97@gmail.com>
2017-10-12 21:59:41 +0530

    To find of datatype

---
+++ size of datatype.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+   {
+    int i;
+    float f;
+    double d;
+    char c;
+    long int l;
+    printf("size of int is=%d\n",sizeof(int));
+    printf("size of float is=%d\n",sizeof(float));
+    printf("size of double is=%d\n",sizeof(double));
+    printf("size of char is=%d\n",sizeof(char));
+    printf("size of long int is=%d\n",sizeof(long int));
+    printf("size of long int is=%1d\n",sizeof(long double));
+   }
